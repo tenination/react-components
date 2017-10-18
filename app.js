65891c7ce191c2b41bb9bee508c326e2f91b94e8
Tim Ninan <tim.ninan@gmail.com>
2017-10-18 17:29:23 -0500

    completed all basic requirements

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// TODO
+var App = () => (
+  <div>
+  	<h2>Grocery List</h2>
+  	<GroceryList items={['eggs', 'milk', 'chocolate']} />
+  </div>
+);
+
+var GroceryList = (props) => (
+		<ul>
+			{
+				props.items.map(item => 
+					<GroceryListItem item={item} />
+			)}
+		</ul>
+);
+
+
+class GroceryListItem extends React.Component {
+
+	constructor(props) {
+    // Equivalent to ES5's React.Component.call(this, props)
+    super(props);
+    this.state = {
+    	done: false
+    };
+  }
+  onItemClick() {
+  	this.setState({
+  		done: !this.state.done
+  	});
+  }
+  render() {
+  	var style = {
+  		textDecoration: this.state.done ? 'line-through' : 'none',
+  		color: this.state.done ? 'blue' : 'blank'
+  	};
+    return (
+      <li style={style} onClick={this.onItemClick.bind(this)}>{this.props.item}</li> 
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
---
+++ app.jsx
@@ -0,0 +1,9 @@
+// TODO
+var GroceryList = () => (
+	<ul>
+		<li>Eggs</li>
+		<li>Milk</li>
+	</ul>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
